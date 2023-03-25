8b2bc429b542d4558e5824e157915a1b14964c70
maybejay <maybejaycodes@hotmail.com>
2023-03-25 13:13:35 -0400

    initial commit

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import styles from "@wall/components/header/header.module.css";
+
+function Header() {
+	return (
+		<div className={`${styles.container} wall-header`}>
+			Header
+		</div>
+	);
+}
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@wall/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Header from '@wall/components/header/header';
+import '@wall/styles/globals.module.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div>
+			<style jsx global>{`
+				body { margin: 0; }
+			`}</style>
+			<Header />
+			< Component {...pageProps} />;
+		</div>
+	);
 }
